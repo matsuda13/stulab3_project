1ffa5309eebf426b2273c6872b1a56ec974a4acf
matsuda13 <genkokurei3@gmail.com>
2022-06-27 13:36:52 +0900

    make main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import sys
+import os
 from TwitterScraping import Twitterscraping
 from extract_content import extract_content
+from negaposi import negaposi
 
 def main():
     args = sys.argv
-    Twitterscraping()
-    extract_content()
-    
+    if not os.path.exists("./out"):
+        print("twitter scraping...")
+        Twitterscraping()
+    else:
+        print("skip scraping.")
+    if not os.path.exists("./out/content.csv"):
+        print("extracting...")
+        extract_content()
+    else:
+        print("skip extract.")
+    print("judging...")
+    try:
+        negaposi()
+        print("complete!")
+    except:
+        print("something went wrong.")
     
     
 
--- negaposi.py
+++ negaposi.py
@@ -5,8 +5,8 @@ from oseti.oseti import Analyzer
 import pandas as pd
 
 
-def judgement(text):
-    ana = oseti.Analyzer()
+def judge(text):
+    ana = Analyzer()
     review = ana.analyze(text)
     re_view= np.average(review)
     return re_view
@@ -15,9 +15,15 @@ def judgement(text):
 #print(ana.analyze_detail('私はとっても幸せ'))
 #print(ana.analyze_detail('私はとっても不幸'))
 
-if __name__ == "__main__":
+def negaposi():
     out_dir = "./out/"
     df = pd.read_csv(out_dir+"content.csv")
-    df['negaposi'] = df['content'].map(judgement)
+    for t in df["content"]:
+        if t==0:
+            print("空白")
+    df['negaposi'] = df['content'].map(judge)
     df = df[df["negaposi"]<0]
     df.to_csv(out_dir+"nega.csv",index=False)
+
+if __name__ == "__main__":
+    negaposi()
